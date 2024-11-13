5178827c89eddbbabbb2d660dcb7eafa291d9543
BLNKSPCE <chasetrower@yahoo.com>
2024-11-12 20:49:26 -0600

    Meals Controller update, needs help

--- Controllers/MealsController.cs
+++ Controllers/MealsController.cs
@@ -42,7 +42,8 @@ public class MealsController : Controller
     //    mealVM.meals = CtrlModel.getMeals();
     //    return View(mealVM);
     //}
-    public IActionResult Create()
+    [HttpGet]
+    public IActionResult Create(string msg = "")
     {
         //If user isn't logged in, don't allow access to this page - redirect to main site page
         AuthToken at;
@@ -58,8 +59,40 @@ public class MealsController : Controller
         {
             return RedirectToAction("Index", "Home");
         }
+        MealsCreateVM cavm = new MealsCreateVM { msg = msg, meal = new Models.MealSet(), UserRecipesNames = CtrlModel.GetRecipeNameList(at.username) };
+        return View(cavm);
+    }
+    [HttpPost]
+    public IActionResult Create(MealsCreateVM MealsVM)
+    {
+        Console.WriteLine("Creating Meal: \n" +
+            "Name: " + MealsVM.meal.Name);
+        AuthToken at;
+        bool test;
+        try
+        {
+            at = JsonConvert.DeserializeObject<AuthToken>(HttpContext.Session.GetString("authToken")!)!;
+            Console.WriteLine("User " + at.username + " Deserialized");
+            test = CtrlModel.CreateMeal(at.username, MealsVM.meal);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error: Exception " + e);
+            test = false;
+        }
 
-        return View();
+        if (!test)
+        {
+            Console.WriteLine("Meal Creation Failure. Sending Error MEssage to View.");
+            return Create("Error, meal could not be created.");
+        }
+        // Uncomment the following line if you want to add to seed data
+        // RecipeSeedData.cookbooks.Add(newCookbook);
+
+        // For now, you can integrate with a database or just return a success view
+        // The index will display the new cookbook in the list, so it's fine.
+        Console.WriteLine("Meal creation success. Redirecting to Meal Index");
+        return RedirectToAction("Index"); // Redirect back to index
     }
 
     public IActionResult Look(string id)
